b6ea80b23925fc3db6148621bcc183cbcf5ce9c7
Çağlar Kutlu Yiğit <caglarkutluyigit@gmail.com>
2023-03-17 01:23:37 +0300

    added: LayerHelper

---
+++ cky-TownRush/Assets/Scripts/Helpers/LayerHelper.cs
@@ -0,0 +1,9 @@
+
+namespace TownRush.Helpers
+{
+    public class LayerHelper
+    {
+        public const string CLICKABLE_TILE = "ClickableTile";
+        public const string NONCLICKABLE_TILE = "NonClickableTile";
+    }
+}
